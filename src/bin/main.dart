b2846d89a8d66aeb457a36d6a29f54bee9272723
James Dooley <james@developernotes.org>
2023-03-02 14:21:51 +0100

    Added code to handle command line options

--- src/bin/main.dart
+++ src/bin/main.dart
@@ -7,12 +7,85 @@
 //  28-02-2023: Initial version
 //
 // *******************************************************************************************
-import 'dart:io';
+import 'dart:io' as io;
+import 'package:args/args.dart' as args;
 import 'package:src/support.dart' as support;
 import 'package:src/configfile.dart' as cfg;
 
-void main(List<String> args) {
+args.ArgParser getArgsParser() {
+  final argParser = args.ArgParser();
+
+  var cmd = argParser.addCommand("merge");
+  cmd.addOption("file", abbr: "f");
+
+  cmd = argParser.addCommand("split");
+  cmd.addOption("file", abbr: "f");
+
+  argParser.addFlag("help", abbr: "h", negatable: false);
+  argParser.addFlag("version", abbr: "v", negatable: false);
+
+  return argParser;
+}
+
+void mergeRoutes({required io.File sourceFile}) {
+  print("Merge file: ${sourceFile.toString()}");
+}
+
+void splitFile({required io.File sourceFile}) {
+  print("Split file: ${sourceFile.toString()}");
+}
+
+void displayHelp() {
+  print("Display help");
+}
+
+void displayVersion() {
+  print("Display version");
+}
+
+void main(List<String> options) {
+  // Get the configuration parameters
   final cfg.ConfigFile config = cfg.ConfigFile(support.getConfigFile(appName: 'gpx_utils'));
   print(config.toString());
-  exit(0);
+
+  // Get the options selected by the user
+  final argsParser = getArgsParser();
+
+  args.ArgResults results;
+
+  try {
+    results= argsParser.parse(options);
+  } on FormatException {
+    displayHelp();
+    io.exit(255);
+  }
+
+  // Determine the command selected
+  final version = results['version'] ?? false;
+  if (version) {
+    displayVersion();
+    io.exit(0);
+  }
+
+  final help = results['help'] ?? false;
+  if (help) {
+    displayHelp();
+    io.exit(0);
+  }
+
+  final cmd = results.command;
+  if (cmd != null) {
+    switch (cmd.name) {
+      case 'merge':
+        final fileName = cmd["file"];
+        mergeRoutes(sourceFile: io.File(fileName));
+        break;
+      case 'split':
+        final fileName = cmd["file"];
+        splitFile(sourceFile: io.File(fileName));
+        break;
+    }
+  }
+
+  io.exit(0);
 }
--- src/lib/configfile.dart
+++ src/lib/configfile.dart
@@ -8,11 +8,23 @@
 //
 // *******************************************************************************************
 import 'dart:io' as io;
+import 'package:toml/toml.dart' as toml;
 
 class ConfigFile {
   final io.File _file;
 
-  ConfigFile(this._file);
+  ConfigFile(this._file) {
+    if (_file.existsSync()) {
+      // If the file exists we load the contents and use to config the application
+      final document = toml.TomlDocument.load(_file.toString());
+    } else {
+      // If no config file is found we fall back on the default values
+    }
+  }
+
+  bool save() {
+    return false;
+  }
 
   String get fileName => _file.toString();
 
